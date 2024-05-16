e37c7a0e4dc60319414549060b4a8d35389b9f15
rengasayee <rsrinivasarengasayee@gmail.com>
2024-05-16 21:34:29 +0530

    Added link to respective section in the page when button is clicked in navbar, BackToTop button added, adjusted background of navbar links, added border to menu headings

--- file.js
+++ file.js
@@ -115,4 +115,33 @@ starterclick.addEventListener('click',() => {
 //     console.log(weekend.innerText);
 // }
 
+let mybutton = document.getElementById("backtotop");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// mybutton.addEventListener('click', () => {
+//     let top = document.getElementById('topbutton');
+//     top.innerHTML += `<a href="#home"></a>`;
+// })
+
+// mybutton.addEventListener('click', () => {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// })
+    
